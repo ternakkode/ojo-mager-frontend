fead302be1503b9f2e65005131f8aa72cc61d3fd
argosatrio <argosatrio30@gmail.com>
2021-03-10 18:01:34 +0700

    feature artikel and footer

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(registrasiRoutes);
 app.use(artikelRoutes);
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3005;
 app.listen(port, () => {
     console.log(`Server is listening on http://localhost:${port}`);
 })
