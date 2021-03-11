9a2d8de971424f2735ac42527dcff274d678f666
helmiagustiawan905 <helmiagustiawan905@gmail.com>
2021-03-11 08:27:46 +0700

    [Helmi] update list olahraga

--- routes/programLatihan.js
+++ routes/programLatihan.js
@@ -5,8 +5,12 @@ const express = require('express');
 const router = express.Router();
 
 const detailProgramControllers = require('./../controllers/programLatihan/detailLatihan');
+const listOlahragaControllers = require('./../controllers/programLatihan/listOlahraga');
 
-// login
+// detail-olahraga
 router.get('/detail-olahraga', detailProgramControllers.getDetailProgram);
+
+// list-olahraga
+router.get('/list-olahraga', listOlahragaControllers.getListProgram);
 
 module.exports = router;
