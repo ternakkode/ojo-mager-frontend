801a1a25848c2848bd597e03beaf81d97a6eaa1b
argosatrio <argosatrio30@gmail.com>
2021-03-13 18:30:31 +0700

    footer header slicing

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(programsRoutes);
 app.use(articlesRoute);
 app.use(errorController.errorPage);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => {
     console.log(`sever listening to http://localhost:${PORT}`);
 });
